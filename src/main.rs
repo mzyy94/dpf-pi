5092d554c83e8c04323b6278f771a5328b825d3b
Yuki MIZUNO <mizuyuu0904@gmail.com>
2021-04-13 23:57:42 +0900

    Add basic struct

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,26 @@
 #![allow(dead_code)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+struct Image {
+    data: Vec<u8>,
+    width: u32,
+    height: u32,
+}
+
+struct Element {
+    component: *mut COMPONENT_T,
+    handle: OMX_HANDLETYPE,
+    in_port: u32,
+    out_port: u32,
+}
+
+struct Pipeline {
+    client: *mut ILCLIENT_T,
+    buffer_header: *mut OMX_BUFFERHEADERTYPE,
+    render: Element,
+    resize: Element,
+}
+
 fn main() {
     println!("Hello, world!");
 }
