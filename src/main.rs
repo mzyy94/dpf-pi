7afd1e8fe7a274d290e8f8d30f172bb0e906829a
Yuki MIZUNO <mizuyuu0904@gmail.com>
2021-04-14 21:19:36 +0900

    Add struct initialize methods

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@
 #![allow(dead_code)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+use std::mem::zeroed;
+use std::os::raw::c_void;
+
 struct Image {
     data: Vec<u8>,
     width: u32,
@@ -17,11 +20,37 @@ struct Element {
     out_port: u32,
 }
 
+impl Default for Element {
+    fn default() -> Self {
+        Self {
+            component: &mut COMPONENT_T { _unused: [] },
+            handle: 0 as *mut c_void,
+            in_port: 0,
+            out_port: 0,
+        }
+    }
+}
+
 struct Pipeline {
     client: *mut ILCLIENT_T,
     buffer_header: *mut OMX_BUFFERHEADERTYPE,
     render: Element,
     resize: Element,
+}
+
+impl Pipeline {
+    fn new() -> Pipeline {
+        unsafe {
+            let client = ilclient_init();
+
+            Pipeline {
+                client: client,
+                buffer_header: zeroed(),
+                render: Default::default(),
+                resize: Default::default(),
+            }
+        }
+    }
 }
 
 fn main() {
