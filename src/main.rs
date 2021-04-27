f72b926696f4b7c942fab1b261907b8a15c2b842
Yuki MIZUNO <mizuyuu0904@gmail.com>
2021-04-28 03:04:01 +0900

    Add graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,12 @@ use hyper::service::{make_service_fn, service_fn};
 use hyper::Server;
 use std::sync::{Arc, Mutex};
 
+async fn shutdown_signal() {
+    tokio::signal::ctrl_c()
+        .await
+        .expect("Failed to install signal handler");
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     omx::init();
@@ -28,7 +34,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     let server = Server::bind(&addr).serve(service);
     println!("Listening on http://{}", addr);
-    server.await?;
+    let graceful = server.with_graceful_shutdown(shutdown_signal());
+    if let Err(e) = graceful.await {
+        eprintln!("Server error: {}", e);
+    }
 
     pipeline.lock().unwrap().destroy();
     omx::deinit();
